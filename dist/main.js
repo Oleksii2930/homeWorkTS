a2bc7fc87c66164d17358abf38d6242a0042c0e5
Олексій2930 <korobcukdenis30@gmail.com>
2025-08-29 12:15:01 +0300

    lesson1/2

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,36 @@
-let a = 'hello world';
-let arr = [];
+// let a: string = 'hello world';
+// let arr: number[] = [];
+//1
+// type UserType = {
+//     name: string,
+//     age: number,
+//     status: boolean,
+//     greeting: (str: string) => string,
+// }
+//2
+// interface UserType  {
+//     name: string,
+//     age: number,
+//     status: boolean,
+//     greeting: (str: string) => string,
+// }
+// let users: UserType[] = [
+//     {greeting: (str) => 'hello', name: 'vasya', age: 21, status: false},
+//     {greeting: (str) => 'hello2', name: 'oleh', age: 23, status: false}
+// ];
+//3
+class User {
+    constructor(name, age, status) {
+        this.name = name;
+        this.age = age;
+        this.status = status;
+    }
+    greeting() {
+        console.log('Hello World!');
+    }
+}
+let user = new User('asd', 21, false);
+let users = [
+    new User('asdfg', 23, true)
+];
+console.log(users);
--- lesson1/main.ts
+++ lesson1/main.ts
@@ -1,13 +1,45 @@
 // let a: string = 'hello world';
 // let arr: number[] = [];
 
-type UserType = {
-    name: string,
-    age: number,
-    status: boolean,
-    greeting: (str: string) => string,
+//1 типи
+// type UserType = {
+//     name: string,
+//     age: number,
+//     status: boolean,
+//     greeting: (str: string) => string,
+// }
+
+//2 інтерфейси
+// interface UserType  {
+//     name: string,
+//     age: number,
+//     status: boolean,
+//     greeting: (str: string) => string,
+// }
+// let users: UserType[] = [
+//     {greeting: (str) => 'hello', name: 'vasya', age: 21, status: false},
+//     {greeting: (str) => 'hello2', name: 'oleh', age: 23, status: false}
+// ];
+
+//3
+class User  {
+    name: string;
+    age: number;
+    status: boolean;
+constructor(name: string, age: number, status: boolean) {
+        this.name = name;
+        this.age = age;
+        this.status = status;
 }
-let users: UserType[] = [
-    {greeting: (str) => 'hello', name: 'vasya', age: 21, status: false},
-    {greeting: (str) => 'hello2', name: 'oleh', age: 23, status: false}
+
+
+    greeting() {
+    console.log('Hello World!');
+    }
+}
+let user: User = new User('asd', 21, false);
+
+let users: User[] = [
+  new User('asdfg', 23, true)
 ];
+console.log(users);
